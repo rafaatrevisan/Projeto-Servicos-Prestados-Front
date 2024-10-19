b5fb369cf0924723d4824d6394fc9156075dc1e4
rafaatrevisan <rafa_tre2@hotmail.com>
2024-10-19 02:02:10 -0300

    Alterando cadastro de usuário

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,7 +8,8 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent  {
 
-  username: string;
+  name: string;
+  email: string;
   password: string;
   loginError: boolean;
   cadastrando: boolean;
