4daa737813cc4c9e185659942d39e2df4bab919f
rafaatrevisan <rafa_tre2@hotmail.com>
2024-10-19 06:24:50 -0300

    Finalizando o cadastro de usuários

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Usuario } from './usuario';
+import { AuthService } from '../auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +11,14 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent  {
 
-  name: string;
+  nome: string;
   email: string;
-  password: string;
-  loginError: boolean;
+  senha: string;
   cadastrando: boolean;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   onSubmit(){
@@ -30,6 +33,39 @@ export class LoginComponent  {
   cancelaCadastro(event){
     event.preventDefault();
     this.cadastrando = false;
+  }
+
+  cadastrar(){
+    const usuario: Usuario = new Usuario();
+    usuario.nome = this.nome;
+    usuario.email = this.email;
+    usuario.senha = this.senha;
+
+    this.authService
+    .salvar(usuario)
+    .subscribe(response => {
+      Swal.fire({
+        icon: "success",
+        title: "Usuário cadastrado com sucesso!",
+        text: "Por favor, efetue o login.",
+        showConfirmButton: false,
+        timer: 1750
+      });
+      this.cadastrando = false;
+      this.nome = '';
+      this.email = '';
+      this.senha = '';
+    }, errorResponse => {
+      let errors = errorResponse.error.errors.join('<br>');
+      
+      Swal.fire({
+        icon: "error",
+        title: "Erro ao tentar cadastrar usuário!",
+        html: errors,
+        showConfirmButton: true,
+        confirmButtonColor: "#f27474"
+      });
+    })
   }
 
 }
